000cad695f660a4d84162c450b3143eb2da98e0c
shixin <shixin217@126.com>
2020-03-11 21:07:59 +0800

    记住登录密码

--- lib/routes/mine.dart
+++ lib/routes/mine.dart
@@ -25,3 +25,14 @@ class _MinePageState extends State<MinePage> {
 
   }
 }
+
+
+class UserHeaderItem extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+
+}
