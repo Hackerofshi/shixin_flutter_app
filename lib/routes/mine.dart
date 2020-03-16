1e16a85da806c17ddd4541681650cc977f3cbf37
shixin <shixin217@126.com>
2020-03-16 21:18:36 +0800

    我的页面

--- lib/routes/mine.dart
+++ lib/routes/mine.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:shixin_flutter_app/models/userinfo.dart';
 import 'package:shixin_flutter_app/models/UserOrg.dart';
+import 'package:shixin_flutter_app/widget/card_item.dart';
 
 class MinePage extends StatefulWidget {
   final String title;
@@ -46,37 +47,59 @@ class UserHeaderItem extends StatelessWidget {
   UserHeaderItem(this.userInfo, this.beStaredCount, this.themeColor,
       {this.notifyColor, this.refreshCallBack, this.orgList});
 
-  _getNotifyIcon(BuildContext context,Color color){
-    if(notifyColor == null){
+  _getNotifyIcon(BuildContext context, Color color) {
+    if (notifyColor == null) {
       return Container();
     }
     return RawMaterialButton(
       materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
       padding: EdgeInsets.only(top: 0.0, right: 5.0, left: 5.0),
-      constraints: BoxConstraints(minWidth: 0.0,minHeight: 0.0),
+      constraints: BoxConstraints(minWidth: 0.0, minHeight: 0.0),
       child: new ClipOval(
         child: new Icon(
-          IconData(0xe600,fontFamily: 'wxcIconFont'),
+          IconData(0xe600, fontFamily: 'wxcIconFont'),
           color: color,
           size: 18.0,
         ),
       ),
-      onPressed: (){
+      onPressed: () {
 
       },
     );
   }
 
 
-
-
-
   @override
   Widget build(BuildContext context) {
-
     // TODO: implement build
 
-    return null;
+    return AndroidCardItem(
+      color: themeColor,
+      elevation: 0,
+      margin: EdgeInsets.all(0.0),
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(0.0),
+              bottomRight: Radius.circular(0.0)
+          )
+      ),
+      child: Padding(padding: EdgeInsets.only(
+          left: 10.0, top: 10.0, right: 10.0, bottom: 0.0
+      ),
+        child:Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 
 }
--- lib/widget/card_item.dart
+++ lib/widget/card_item.dart
@@ -1,11 +1,35 @@
-
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
-class AndroidCardItem extends StatelessWidget{
+class AndroidCardItem extends StatelessWidget {
+  final Widget child;
+  final EdgeInsets margin;
+  final Color color;
+  final RoundedRectangleBorder shape;
+  final double elevation;
+
+  const AndroidCardItem({Key key, this.margin, this.color, this.shape,
+    this.elevation = 5.0, this.child}) : super(key: key);
+
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return null;
-  }
+    EdgeInsets margin = this.margin;
+    RoundedRectangleBorder shape = this.shape;
+    Color color = this.color;
 
+    margin??=EdgeInsets.only(left: 10.0, top: 10.0, right: 10.0, bottom: 10.0);
+    shape ??= new RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(4.0))
+    );
+    color ??=new Color(0xFF24292E);
+    // TODO: implement build
+    return new Card(
+      elevation: elevation,
+      shape: shape,
+      color: color,
+      margin: margin,
+      child: child,
+    );
+  }
 }
